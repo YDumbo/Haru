da123ab1c44023ff8cbf0b9ec708583d6af00364
Yoon <cksal5911@kakao.com>
2021-04-08 20:38:02 +0900

    [UPDATE] Styled Button Component
    type setting

--- components/common/StyledButton.tsx
+++ components/common/StyledButton.tsx
@@ -1,41 +1,23 @@
 import styled from 'styled-components';
-
-type Tcolor = 'green' | 'blue' | 'red'
-type TSize = 'small' | 'medium' | 'large'
-type Tvariant = 'outline' | 'fill-none' | 'fill'
-
-interface IStyledButton {
-  color?: Tcolor,
-  size?: TSize,
-  variant?: Tvariant,
-}
-
-interface IStyledButtonProps {
-  color?: Tcolor,
-  size?: TSize,
-  text: string,
-  label? : React.ReactNode,
-  variant?: Tvariant,
-}
+import { IStyledButton, IStyledButtonProps } from '../../types/styledButton';
 
 const Button = styled.button<IStyledButton>`
   display: flex;
   align-items: center;
   justify-content: space-around;
-  background-color: ${(props) => (props.variant === 'fill' ? props.theme.color[props.color] : 'none')};
-  border: ${(props) => (props.variant === 'outline' ? `1px solid ${props.theme.color[props.color]}` : 'none')};
-  color: ${(props) => (props.variant === 'fill' ? 'white' : props.theme.color[props.color])};;
+  background-color: ${({ variant, theme, color }) => (variant === 'fill' ? theme.color[color] : 'none')};
+  border: ${({ variant, theme, color }) => (variant === 'outline' ? `1px solid ${theme.color[color]}` : 'none')};
+  color: ${({ variant, theme, color }) => (variant === 'fill' ? 'white' : theme.color[color])};;
   padding: 10px;
   border-radius: 3px;
-  width: ${(props) => props.theme.buttonSize[props.size]};
-  cursor: pointer;
+  width: ${({ theme, size }) => theme.buttonSize[size]};
 `;
 
 function StyledButton({
-  color = 'green', size = 'medium', text, label, variant = 'fill',
+  color = 'green', size = 'medium', text, label, variant = 'fill', onClick,
 }: IStyledButtonProps):JSX.Element {
   return (
-    <Button type="button" color={color} size={size} variant={variant}>
+    <Button type="button" color={color} size={size} variant={variant} onClick={onClick}>
       {text} {label}
     </Button>
   );
---
+++ types/styledButton.ts
@@ -0,0 +1,18 @@
+export type Tcolor = 'green' | 'blue' | 'red'
+export type TSize = 'small' | 'medium' | 'large'
+export type Tvariant = 'outline' | 'fill-none' | 'fill'
+
+export interface IStyledButton {
+  color?: Tcolor,
+  size?: TSize,
+  variant?: Tvariant,
+}
+
+export interface IStyledButtonProps {
+  color?: Tcolor,
+  size?: TSize,
+  text: string,
+  label? : React.ReactNode,
+  variant?: Tvariant,
+  onClick?: () => void;
+}
