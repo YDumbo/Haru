374071510afd8a9a41575858286fb2f3805e88c1
Yoon <cksal5911@kakao.com>
2021-04-12 16:21:19 +0900

    [Update] HeatMap Color

--- components/mypage/HeatMap.tsx
+++ components/mypage/HeatMap.tsx
@@ -9,15 +9,37 @@ interface IHashMapProps {
 interface IStyleRect{
   level: number;
 }
+interface IYearItemProps{
+  select: boolean,
+}
+const HeatMapWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
 const GrassBoxWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
+`;
+const YearList = styled.ul`
+  display: flex;
+  flex-direction: column;
+`;
+const YearItem = styled.li<IYearItemProps>`
+  color: ${({ theme }) => theme.color.blue};
+  width: 100px;
+  padding: 10px;
+  text-align: center;
+  font-size: 13px;
+  border-bottom: 1px solid ${({ select, theme }) => (select ? theme.color.blue : theme.color.white)};
+  &:hover {
+    background-color: ${({ select, theme }) => (select === false && theme.color.lightGrey)}; 
+  }
 `;
 const SvgGrassBox = styled.svg`
   overflow: hidden;
-  width: 100%;
   height: 112px;
+  width: 750px;
 `;
 const ColorStateBox = styled.div`
   display: flex;
@@ -32,10 +54,10 @@ const ColorStateBox = styled.div`
 const Week = styled.g``;
 const Day = styled.rect<IStyleRect>`
   fill: ${(props) => (props.level === 0 ? '#ebedf0'
-    : props.level === 1 ? '#9be9a8'
-      : props.level === 2 ? '#40c463'
-        : props.level === 3 ? '#30a14e'
-          : props.level === 4 && '#216e39')
+    : props.level === 1 ? '#9bb1e9'
+      : props.level === 2 ? '#4054c4'
+        : props.level === 3 ? '#3041a1'
+          : props.level === 4 && '#21306e')
 };
 `;
 
@@ -45,51 +67,56 @@ function HeatMap({ currentYear, commitData }: IHashMapProps):JSX.Element {
     setCommit(commitDataParser({ currentYear, commitData }));
   }, []);
   return (
-    <GrassBoxWrapper>
-      <p>{`${currentYear}년 나의 기록들`}</p>
-      <SvgGrassBox>
-        <g>
-          {commit.length !== 0 && new Array(52).fill(0).map((_el, i) => (
-            <Week transform={`translate(${i * 14}, 0)`} key={i}>
-              {new Array(7).fill(0).map((_, index) => {
-                const current = i * 7;
-                const count = commit[current + index].commit;
-                const level = count <= 3 ? count : 4;
-                return (
-                  <Day
-                    key={index}
-                    y={index * 13}
-                    rx={2} ry={2}
-                    width={10}
-                    height={10}
-                    level={level}
-                  />
-                );
-              })}
-            </Week>
-          ))}
-        </g>
-      </SvgGrassBox>
-      <ColorStateBox>
-        <div>Less</div>
-        <svg width={10} height={10}>
-          <Day width={10} height={10} rx={2} ry={2} level={0} />
-        </svg>
-        <svg width={10} height={10}>
-          <Day width={10} height={10} rx={2} ry={2} level={1}/>
-        </svg>
-        <svg width={10} height={10}>
-          <Day width={10} height={10} rx={2} ry={2} level={2}/>
-        </svg>
-        <svg width={10} height={10}>
-          <Day width={10} height={10} rx={2} ry={2} level={3}/>
-        </svg>
-        <svg width={10} height={10}>
-          <Day width={10} height={10} rx={2} ry={2} level={4}/>
-        </svg>
-        <div>More</div>
-      </ColorStateBox>
-    </GrassBoxWrapper>
+    <HeatMapWrapper>
+      <GrassBoxWrapper>
+        <p>{`${currentYear}년 나의 기록들`}</p>
+        <SvgGrassBox>
+          <g>
+            {commit.length !== 0 && new Array(52).fill(0).map((_el, i) => (
+              <Week transform={`translate(${i * 14}, 0)`} key={i}>
+                {new Array(7).fill(0).map((_, index) => {
+                  const current = i * 7;
+                  const count = commit[current + index].commit;
+                  const level = count <= 3 ? count : 4;
+                  return (
+                    <Day
+                      key={index}
+                      y={index * 13}
+                      rx={2} ry={2}
+                      width={10}
+                      height={10}
+                      level={level}
+                    />
+                  );
+                })}
+              </Week>
+            ))}
+          </g>
+        </SvgGrassBox>
+        <ColorStateBox>
+          <div>Less</div>
+          <svg width={10} height={10}>
+            <Day width={10} height={10} rx={2} ry={2} level={0} />
+          </svg>
+          <svg width={10} height={10}>
+            <Day width={10} height={10} rx={2} ry={2} level={1}/>
+          </svg>
+          <svg width={10} height={10}>
+            <Day width={10} height={10} rx={2} ry={2} level={2}/>
+          </svg>
+          <svg width={10} height={10}>
+            <Day width={10} height={10} rx={2} ry={2} level={3}/>
+          </svg>
+          <svg width={10} height={10}>
+            <Day width={10} height={10} rx={2} ry={2} level={4}/>
+          </svg>
+          <div>More</div>
+        </ColorStateBox>
+      </GrassBoxWrapper>
+        <YearList>
+          <YearItem select={true}>2021</YearItem>
+        </YearList>
+    </HeatMapWrapper>
   );
 }
 
